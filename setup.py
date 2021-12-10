742a0fdef1d877fc0f5a14b70ce6585d59bf0c2f
mnicnc404 <mnicnc404@gmail.com>
2021-12-10 10:21:08 +0800

    fix: email in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="PyToyTorch",
     version="0.1",
     author="mnicnc404",
-    author_email="mnicnc404",
+    author_email="mnicnc404@gmail.com",
     packages=find_namespace_packages(include=("PyToyTorch.*")),
     install_requires=[
         'numpy',
